e58da82d2cdd2d23caf4f43b27f42ad6fac156be
Mebarek <mohammedmecheter@gmail.com>
2025-09-19 15:08:13 +0100

    initial commit

---
+++ apps/server/src/lib/auth.ts
@@ -0,0 +1,48 @@
+import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { polar, checkout, portal } from "@polar-sh/better-auth";
+import { polarClient } from "./payments";
+import { db } from "../db";
+import * as schema from "../db/schema/auth";
+import { env } from "cloudflare:workers";
+
+export const auth = betterAuth<BetterAuthOptions>({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+
+		schema: schema,
+	}),
+	trustedOrigins: [env.CORS_ORIGIN],
+	emailAndPassword: {
+		enabled: true,
+	},
+	secret: env.BETTER_AUTH_SECRET,
+	baseURL: env.BETTER_AUTH_URL,
+	advanced: {
+		defaultCookieAttributes: {
+			sameSite: "none",
+			secure: true,
+			httpOnly: true,
+		},
+	},
+	plugins: [
+		polar({
+			client: polarClient,
+			createCustomerOnSignUp: true,
+			enableCustomerPortal: true,
+			use: [
+				checkout({
+					products: [
+						{
+							productId: "your-product-id",
+							slug: "pro",
+						},
+					],
+					successUrl: env.POLAR_SUCCESS_URL,
+					authenticatedUsersOnly: true,
+				}),
+				portal(),
+			],
+		}),
+	],
+});
